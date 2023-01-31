47eb38c4ebe92fd0dd418c57168ddb83cdcf3463
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-01-31 23:19:56 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=co32e2d7";
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 function App() {
   return <h1> My Movie App</h1>;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Iron Men");
+  }, []);
 }
 export default App;
