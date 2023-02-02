bb7549ed4a696c62ea54b05e4729f7fc175fab84
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-02 16:10:50 +0100

    {movie1.Type}

--- src/App.js
+++ src/App.js
@@ -40,8 +40,21 @@ function App() {
           <div>
             <p>{movie1.Year} </p>
           </div>
+
           <div>
-            <img src={movie1.Poster} alt={movie1.Title} />
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+
+          <div>
+            <span>{movie1.Type} </span>
+            <h3>{movie1.Title} </h3>
           </div>
         </div>
       </div>
