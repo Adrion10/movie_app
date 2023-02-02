d92aa86dbc05bd8b68de44e140172a46d16f18db
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-02 16:15:58 +0100

    MovieCard.jsx

--- src/App.js
+++ src/App.js
@@ -35,29 +35,7 @@ function App() {
         />
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year} </p>
-          </div>
-
-          <div>
-            <img
-              src={
-                movie1.Poster !== "N/A"
-                  ? movie1.Poster
-                  : "https://via.placeholder.com/400"
-              }
-              alt={movie1.Title}
-            />
-          </div>
-
-          <div>
-            <span>{movie1.Type} </span>
-            <h3>{movie1.Title} </h3>
-          </div>
-        </div>
-      </div>
+      <div className="container"></div>
     </div>
   );
 }
