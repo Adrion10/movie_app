948b9bf564a54751fb1a51c39e4a530a84963d31
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-02 15:28:47 +0100

    div className="app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
+import SearchIcon from "./Search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
@@ -14,6 +15,14 @@ function App() {
     searchMovies("Iron Men");
   }, []);
 
-  return <h1> My Movie App</h1>;
+  return;
+  (<div className="app"></div>)(<h1> LaceMovieApp</h1>);
+  <div className="search">
+    <input
+      placeholder="Search for movies"
+      value="Iron man"
+      onChange={() => {}}
+    />
+  </div>;
 }
 export default App;
